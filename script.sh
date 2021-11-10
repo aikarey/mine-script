0f480a1433bb6077fd543906d4991563293ed628
aikarey <43464271+aikarey@users.noreply.github.com>
2021-11-11 05:51:06 +0700

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,3 @@
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.24/cpuminer-opt-linux.tar.gz
+tar xvzf cpuminer-opt-linux.tar.gz
+./cpuminer-sse2 -a power2b  -o stratum+tcps://stratum-asia.rplant.xyz:17022 -u Mf93h9ocbeiS4q9975y3zTpnhA8dPnxp4Z.worker1
