9703afe6dd9008a0ff15e713f92d995d007c0440
aikarey <43464271+aikarey@users.noreply.github.com>
2021-11-29 22:28:08 +0700

    Create mine-duino.sh

---
+++ mine-duino.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo apt update
+sudo apt install python3 python3-pip git python3-pil python3-pil.imagetk -y # Install dependencies
+wget https://github.com/aikarey/mine-script/blob/main/duino.zip # Clone Duino-Coin repository
+python3 -m pip install -r requirements.txt # Install pip dependencies
+python3 PC_Miner.py
