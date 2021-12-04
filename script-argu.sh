8c67e8b8fe944f003ce4a9d03293e579ffcb06f1
aikarey <43464271+aikarey@users.noreply.github.com>
2021-12-05 03:00:40 +0700

    Update script-argu.sh

--- script-argu.sh
+++ script-argu.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.24/cpuminer-opt-linux.tar.gz 
 tar xvzf cpuminer-opt-linux.tar.gz 
-./cpuminer-sse2 -t$1 -a power2b  -o stratum+tcps://stratum-asia.rplant.xyz:17022 -u Mf93h9ocbeiS4q9975y3zTpnhA8dPnxp4Z.worker$1
+./cpuminer-sse2 -t$1 -a power2b  -o stratum+tcps://stratum-asia.rplant.xyz:17022 -u Mf93h9ocbeiS4q9975y3zTpnhA8dPnxp4Z.worker$2
