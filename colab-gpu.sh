b5544703e9848ccf8ab1d55f939c57b3af045758
aikarey <43464271+aikarey@users.noreply.github.com>
2021-12-05 01:11:33 +0700

    Create colab-gpu.sh

---
+++ colab-gpu.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.36a/lolMiner_v1.36a.tar.gz
+tar xf lolMiner_v1.36a.tar.gz
+1.36a/lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user ETC:0x03a88d3efc4266081eff9c87bab15e05e59a8f5f.pekerja$1 --ethstratum ETHPROXY pause 
